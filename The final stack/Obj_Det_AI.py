6a97a9aa6e167ea5275cc7a878c0d0cf756462b2
Nasos-Anagnostou <nasosanag@gmail.com>
2022-04-19 22:54:42 +0300

    new changes

--- The final stack/Obj_Det_AI.py
+++ The final stack/Obj_Det_AI.py
@@ -7,7 +7,8 @@ detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+detections, extracted_images = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "ela.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"),extract_detected_objects=True)    # add this to get the image ,extract_detected_objects=True
+
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
