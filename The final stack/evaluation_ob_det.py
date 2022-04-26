b7eb464f87bc7e7f11cd50d8bf47d238aaeb4661
Nasos-Anagnostou <nasosanag@gmail.com>
2022-04-26 14:46:57 +0300

    update

--- The final stack/evaluation_ob_det.py
+++ The final stack/evaluation_ob_det.py
@@ -1,18 +1,13 @@
-from imageai.Detection.Custom import DetectionModelTrainer
-
-# 1. Evaluation models
-trainer = DetectionModelTrainer()
-trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="Dataset folder")
-trainer.evaluateModel(model_path="Dataset folder/models", json_path="Dataset folder/json/detection_config.json", iou_threshold=0.5, object_threshold=0.3, nms_threshold=0.5)
-
-
-# # 2. Detection
-# detector = CustomObjectDetection()
-# detector.setModelTypeAsYOLOv3()
-# detector.setModelPath("hololens-ex-60--loss-2.76.h5")
-# detector.setJsonPath("detection_config.json")
+from imageai.Detection import ObjectDetection
+import os
+# execution_path = os.getcwd()
+#
+# detector = ObjectDetection()
+# detector.setModelTypeAsRetinaNet()
+# detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
 # detector.loadModel()
-# detections = detector.detectObjectsFromImage(input_image="ela.jpg", output_image_path="ela-detected.jpg")
-# for detection in detections:
-#     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
+# detections, extracted_images = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "ela.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"),extract_detected_objects=True)    # add this to get the image ,extract_detected_objects=True
+#
+#
+# for eachObject in detections:
+#     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
--- The final stack/readme.txt
+++ The final stack/readme.txt
@@ -9,3 +9,7 @@
 5) csv_fun = script for csv file preprocessing adding event_id column and changing column names
 
 6) event_clip =  creating a videoclip of the event the user chose to see
+
+7) easyOcr = easyOcr OCR engine used to parse timetags for each frame detected
+
+8) Obj_Det_AI  = ROI localization using a a pre-trained YOLOV3 model to generate a custom model for the timebox
